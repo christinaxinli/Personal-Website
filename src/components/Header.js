10f41ceafb042ae17a089de2d0b529b4efad7892
Christina Li <christinali@Christinas-MacBook-Pro.local>
2019-08-31 15:30:17 +0200

    hiding intro until there is a bio

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li> */}
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('music')}}>Music</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('social')}}>Social</a></li>
